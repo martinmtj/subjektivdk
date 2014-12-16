0df00f39a28a9b3090d2d2bf2abbf577b4e2c3ae
Martin Jensen <martin@mtj.dk>
2014-12-16 14:37:40 +0100

    Create plot-csv.sh

---
+++ plot-csv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sqlite3 -header -csv datalog.db "SELECT * FROM temp;" > temp.csv
+
+gnuplot << EOF
+
+set title 'Temperatur'
+set ylabel 'Celcius'
+set xlabel 'Tid'
+set grid
+set term png
+set output 'temperature.png'
+set datafile separator ','
+set xdata time
+set timefmt '%Y-%m-%d %H:%M:%S'
+
+plot 'temp.csv' using 1:3 smooth bezier with lines title 'Indoor' 
+
+EOF
+
+#
+##wput -R plotfile.png ftp://USER:PASSWORD@voresserver.dk/sommerhus/
